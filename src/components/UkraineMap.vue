eb08f85c1707588656b10a79d5e7ed51cfcb9b34
lis-dev <mathfak@gmail.com>
2022-04-20 11:23:06 +0300

    #2 add meta headers

--- src/components/UkraineMap.vue
+++ src/components/UkraineMap.vue
@@ -8,6 +8,16 @@ import SvgUkraineMap from "./svg/UkraineMap.vue";
 import { RegionRepository } from "../repository/RegionRepository";
 import { TelegramRegionStatusService } from "../api/telegram/TelegramRegionStatusService";
 import { Region } from "@/types/Region";
+import { Status } from "@/types/Status";
+
+const inDangerTitle = (count: number) =>
+  count > 0
+    ? `❗${count} regions are in danger right now`
+    : `💛 💙 Looks like all regions are safe`;
+const inDangerDescription = (count: number) =>
+  count > 0
+    ? `❗❗ ${count} are in danger right now`
+    : `Looks like all regions are safe`;
 
 export default defineComponent({
   components: { SvgUkraineMap },
@@ -23,6 +33,27 @@ export default defineComponent({
   data() {
     return {
       regions: [] as Region[],
+    };
+  },
+  metaInfo() {
+    const regionsInADanger = this.regions.filter(
+      (region: Region) => region.status !== Status.OK
+    );
+    const title = inDangerTitle(regionsInADanger.length);
+    const description = inDangerDescription(
+      regionsInADanger.map((region: Region) => region.title).join(", ")
+    );
+
+    return {
+      title,
+      meta: [
+        // TODO Move to config
+        { property: "og:title", content: title },
+        { property: "og:site_name", content: "Alert.Org.Ua" },
+        { property: "og:type", content: "website" },
+        { property: "og:url", content: "https://alert.org.ua/" },
+        { property: "og:description", content: description },
+      ],
     };
   },
 });
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createMetaManager, plugin as vueMetaPlugin } from "vue-meta";
 
-createApp(App).mount("#app");
+createApp(App).use(createMetaManager()).use(vueMetaPlugin).mount("#app");
