7566676c33fdea6ce3ed300e908f126631fa4ef8
lis-dev <mathfak@gmail.com>
2022-04-22 15:42:36 +0300

    #18 add gtag

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createMetaManager, plugin as vueMetaPlugin } from "vue-meta";
+import VueGtag from "vue-gtag";
 
-createApp(App).use(createMetaManager()).use(vueMetaPlugin).mount("#app");
+createApp(App)
+  .use(createMetaManager())
+  .use(vueMetaPlugin)
+  .use(VueGtag, {
+    config: { id: process.env.VUE_APP_GA_MEASUREMENT_ID },
+  })
+  .mount("#app");
